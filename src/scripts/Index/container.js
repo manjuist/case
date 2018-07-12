a511bda49069771eac4af99c90aee91dc2d018c2
deve <deve_m@163.com>
2018-07-12 14:23:55 +0800

    new package

--- src/scripts/Index/container.js
+++ src/scripts/Index/container.js
@@ -3,11 +3,12 @@ import ReactDOM,{render}                             from 'react-dom';
 import {HashRouter as Router,Route,Link,Switch}      from 'react-router-dom';
 import {Provider,connect}                            from 'react-redux';
 import actionCreator from './actionCreater'
+import Loadable from 'react-loadable'
 
 @connect(function(){return {a:1}},{function(){return{a:()=>{console.log(1)}}}})
 class Index extends Component {
     render(){
-        return <div>index{this.props.a}</div>
+        return <div>index{this.props.a}111</div>
     }
 }
 
