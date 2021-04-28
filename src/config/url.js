8d7ea1e56af18da88140e6daa65ea464c6e449c2
dewei.ma <dewei.ma@lvyuetravel.com>
2021-04-28 15:25:24 +0800

    feat: add vis cytoscape timeline

--- src/config/url.js
+++ src/config/url.js
@@ -1,15 +1,13 @@
 const url = { 
     home: { name: 'home', path: '/home' },
     info: { name: 'info', path: '/home/info' },
-    skill: { name: 'skill', path: '/home/skill' },
     profile: { name: 'profile', path: '/home/profile' },
-    project: { name: 'project', path: '/home/project' },
-    other: { name: 'other', path: '/home/other' },
-    work: { name: 'work', path: '/home/work' },
-    tech: { name: 'tech', path: '/home/tech' },
     drawingBoard: { path: '/drawing-board', name: 'drawing-board' },
     flip: { name: 'flip', path: '/flip' },
     g6: { name: 'g6', path: '/g6' },
+    cytoscape: { name: 'cytoscape', path: '/cytoscape' },
+    timeline: { name: 'timeline', path: '/timeline' },
+    vis: { name: 'vis', path: '/vis' },
 }
 
 export default url
--- src/router/home.js
+++ src/router/home.js
@@ -2,23 +2,13 @@ import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import url from 'config/url'
 import Info from 'components/home/info'
-import Other from 'components/home/other'
 import Profile from 'components/home/profile'
-import Project from 'components/home/project'
-import Skill from 'components/home/skill'
-import Tech from 'components/home/tech'
-import Work from 'components/home/work'
 
 function RouteList(){
     return (
         <Switch>
             <Route exact path={url.info.path} component={Info} />
-            <Route exact path={url.other.path} component={Other} />
             <Route exact path={url.profile.path} component={Profile} />
-            <Route exact path={url.project.path} component={Project} />
-            <Route exact path={url.skill.path} component={Skill} />
-            <Route exact path={url.tech.path} component={Tech} />
-            <Route exact path={url.work.path} component={Work} />
             <Redirect to={url.info.path} />
         </Switch>
     )
