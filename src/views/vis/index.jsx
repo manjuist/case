2aa177f6b429197e1c3a2fc179bbbcec2b52ac22
dewei.ma <dewei.ma@lvyuetravel.com>
2021-04-28 15:24:34 +0800

    feat: add vis-network

---
+++ src/views/vis/index.jsx
@@ -0,0 +1,44 @@
+/**
+ * name: index.jsx
+ * author: Deve
+ * date: 2021-04-19
+ */
+
+/* eslint-disable */
+import React, { useEffect } from 'react'
+import vis from './vis'
+
+let count = 10000;
+const nodes = []
+// const num = (min = 0, max = 2000) => min + (((max - min) + 1) * Math.random())
+while (count > 0){
+    count -= 1;
+    nodes.push({
+        id: `n${count}`,
+        label: `n${count}`,
+        // x: num(),
+        // y: num(),
+    })
+}
+
+
+const graphData = { nodes };
+function G6Demo(){
+    const ref = React.createRef();
+    useEffect(() => {
+        console.log(2)
+        const dom = ref.current;
+        // create a network
+        const data = {
+            nodes: new vis.DataSet(nodes),
+        };
+        const options = {};
+        new vis.Network(dom, data, options);
+    }, [])
+
+    return (
+        <div ref={ref} style={{ width: '1000px', height: '1000px' }} />
+    )
+}
+
+export default G6Demo;
